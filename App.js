2c7a70664864baa0ad1838d9c38fd54d93e9b52d
DavyjonesCode <davidinpy@gmail.com>
2020-11-05 11:06:05 -0800

    changes to drawer navigation

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Main from "./screens/main";
 import Menu from "./screens/menu";
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={Main} />
-        <Stack.Screen name="Menu" component={Menu} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Main} />
+        <Drawer.Screen name="Menu" component={Menu} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
--- components/navBar.js
+++ components/navBar.js
@@ -1,37 +1,41 @@
-import React from "react"
-import {View, Text, StyleSheet, Button} from "react-native"
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const NavBar = ({ navigation }) => {
-    return(
-        <View style={styles.container}>
-            <AntDesign name="left" size={35} color="white" />
-            <Text style={styles.text}>Home Page</Text>
-            <MaterialCommunityIcons name="menu" size={35} color="white" onPress={() => navigation.navigate('Menu')}/>
-        </View>
-    )
-}
-
+  return (
+    <View style={styles.container}>
+      <AntDesign name="left" size={35} color="white" />
+      <Text style={styles.text}>Home Page</Text>
+      <MaterialCommunityIcons
+        name="menu"
+        size={35}
+        color="white"
+        onPress={() => navigation.toggleDrawer()}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#111028",
-        bottom: 0,
-        position: "absolute",
-        width: "100%",
-        height: 60,
-        borderTopColor: "white",
-        borderTopWidth: 3,
-        justifyContent: 'center',        
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    text: {
-        fontWeight: 'bold',
-        color: 'white',
-        fontSize: 25,
-    }
-})
+  container: {
+    backgroundColor: "#111028",
+    bottom: 0,
+    position: "absolute",
+    width: "100%",
+    height: 60,
+    borderTopColor: "white",
+    borderTopWidth: 3,
+    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  text: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 25,
+  },
+});
 
-export default NavBar
+export default NavBar;
