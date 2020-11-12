c7e0ec5d54d551e9bbe186b5cc6d8719b37aacdf
DavyjonesCode <davidinpy@gmail.com>
2020-11-12 11:44:09 -0800

    fixes example class color fade

--- screens/ExampleClass.js
+++ screens/ExampleClass.js
@@ -1,33 +1,37 @@
 import React from "react";
-import {View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import NavBar from "../components/navBar";
 import Assignment from "../components/assignment";
 import DATA from "../data/classes";
 
-
 function ExampleClass({ navigation, route }) {
-  const {name} = route.params
-  const data = DATA.find(el => el.title == name).assignments
-  
-  const renderItem = ({ item }) => (
+  const { name } = route.params;
+  const data = DATA.find((el) => el.title == name).assignments;
+  const colorList = [
+    "#BEB2DA",
+    "#B8AAD6",
+    "#B2A2D2",
+    "#A593CB",
+    "#9E8BC7",
+    "#9883C4",
+    "#917BC0",
+    "#8B74BC",
+  ];
+  const renderItem = ({ item, index }) => (
     <Assignment
       assignmentTitle={item.title}
       assignmentDueDate={item.dueDate}
       assignmentGrade={item.grade}
-      newColor={"green"}
-      id={item.id}
+      newColor={colorList[index]}
     />
   );
   return (
     <View style={styles.container}>
       <View>
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-        />
+        <FlatList data={data} renderItem={renderItem} />
       </View>
-      <NavBar navigation={navigation} Directory="Example Class"/>
+      <NavBar navigation={navigation} Directory="Example Class" />
     </View>
   );
 }
